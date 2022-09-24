58e27e9e94fb29c9b3c246c88c3e868c0300b6e4
Enrique Delgado <edelga34@ford.com>
2022-09-24 18:06:10 -0400

    setting up heroku production environment

--- livetakeoff/livetakeoff/settings/prod.py
+++ livetakeoff/livetakeoff/settings/prod.py
@@ -1,8 +1,14 @@
 import os
+import dj_database_url
 from .common import *
 
 DEBUG = False
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['api-livetakeoff.herokuapp.com']
+
+DATABASES = {
+    # This function looks for an environment variable called DATABASE_URL
+    'default': dj_database_url.config()
+}
