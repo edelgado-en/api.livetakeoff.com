18c119dfe621ca2b3dc6dbf260dea7063c32eaa7
Enrique Delgado <edelga34@ford.com>
2022-09-25 13:21:57 -0400

    added retainer service

--- api/models.py
+++ api/models.py
@@ -2,6 +2,18 @@ from django.db import models
 
 class Service(models.Model):
     name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True, null=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+class RetainerService(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True, null=True)
     active = models.BooleanField(default=True)
 
     def __str__(self) -> str:
