9eea8b5a567c960e6af729be5616a54a9cae861f
Enrique Delgado <edelga34@ford.com>
2022-09-25 06:00:19 -0400

    Added static root to settings

--- livetakeoff/livetakeoff/settings/common.py
+++ livetakeoff/livetakeoff/settings/common.py
@@ -11,12 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-
 
 # Application definition
 
@@ -99,6 +97,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
