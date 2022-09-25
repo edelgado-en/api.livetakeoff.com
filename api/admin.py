8b86d891b62f5f140a9587857f15f386dcdc5251
Enrique Delgado <edelga34@ford.com>
2022-09-25 13:24:12 -0400

    registered retainer service

--- api/admin.py
+++ api/admin.py
@@ -11,3 +11,14 @@ class ServiceAdmin(admin.ModelAdmin):
     def has_delete_permission(self, request, obj=None):
         # Disable delete because when you want to stop using a service, just set it to inactive
         return False
+
+@admin.register(models.RetainerService)
+class RetainerServiceAdmin(admin.ModelAdmin):
+    list_display = ['name', 'active']
+    list_per_page = 100
+    ordering = ['name', 'active']
+    search_fields = ['name', 'active']
+
+    def has_delete_permission(self, request, obj=None):
+        # Disable delete because when you want to stop using a service, just set it to inactive
+        return False
