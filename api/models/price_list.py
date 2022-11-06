83f1685bac240d6e889e8601ac2ddfe1f1db8cc2
Enrique Delgado <edelga34@ford.com>
2022-11-06 12:32:37 -0500

    Added created by and created at to price list

--- api/models/price_list.py
+++ api/models/price_list.py
@@ -4,6 +4,8 @@ from django.db import models
 class PriceList(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True, null=True)
+    created_by = models.ForeignKey('auth.User', related_name='pricelists', on_delete=models.PROTECT, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
 
     def __str__(self) -> str:
         return self.name
