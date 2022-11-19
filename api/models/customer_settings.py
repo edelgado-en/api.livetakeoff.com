b547c2f0719de9990bc86d7efc2107ad8c61d97c
Enrique Delgado <edelga34@ford.com>
2022-11-19 14:14:43 -0500

    Adjusted retainer amount max digits to 9

--- api/models/customer_settings.py
+++ api/models/customer_settings.py
@@ -6,7 +6,7 @@ class CustomerSettings(models.Model):
     customer = models.OneToOneField(Customer, on_delete=models.CASCADE, related_name='customer_settings')
     show_spending_info = models.BooleanField(default=False)
     allow_cancel_job = models.BooleanField(default=False)
-    retainer_amount = models.DecimalField(max_digits=6, decimal_places=2, blank=True, null=True)
+    retainer_amount = models.DecimalField(max_digits=9, decimal_places=2, blank=True, null=True)
     #TODO: add retainer_contract as PDF. Configure Cloudinary to allow PDF upload
     show_job_price = models.BooleanField(default=False)
     special_instructions = models.TextField(blank=True, null=True)
