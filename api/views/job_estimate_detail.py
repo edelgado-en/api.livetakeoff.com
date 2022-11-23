f2d4f141d65bbcb77f8a875c5d374aab34bede44
Enrique Delgado <edelga34@ford.com>
2022-11-23 08:16:55 -0500

    added support to update estimate

--- api/views/job_estimate_detail.py
+++ api/views/job_estimate_detail.py
@@ -10,6 +10,9 @@ from api.models import (JobEstimate, JobEstimateDiscount, JobEstimateAdditionalF
 
 from api.serializers import (JobEstimateDetailSerializer,)
 
+from api.notification_util import NotificationUtil
+from api.email_util import EmailUtil
+
 class JobEstimateDetailView(APIView):
     permission_classes = (permissions.AllowAny,)
 
@@ -30,3 +33,42 @@ class JobEstimateDetailView(APIView):
         serializer = JobEstimateDetailSerializer(estimate)
 
         return Response(serializer.data)
+
+
+    def post(self, request, id):
+        estimate = get_object_or_404(JobEstimate, pk=id)
+
+        # if this estimate has been processed already, throw an error
+        if estimate.is_processed:
+            return Response({'error': 'This estimate has already been processed'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+        # update estimate status
+        estimate.status = request.data.get('status')
+        estimate.is_processed = True
+        estimate.processed_at = datetime.now()
+        
+        estimate.save()
+
+       # notify the user who requested the estimate via email and text message
+        notification_util = NotificationUtil()
+
+        # get the phone number of the user who requested the estimate
+        phone_number = estimate.requested_by.profile.phone_number
+
+        status_name = 'REJECTED'
+
+        if estimate.status == 'A':
+            status_name = 'ACCEPTED'
+
+        if phone_number:
+            # send a text message
+            message = f'Your estimate for aircraft {estimate.aircraftType.name} at airport {estimate.airport.initials} has been {status_name}. You can checkout it out at https://livetakeoff.com/estimate/{estimate.id}'
+            notification_util.send(message, phone_number.as_e164)
+
+        
+        # TODO: send an email
+        # email_util = EmailUtil()
+
+
+        return Response(status=status.HTTP_200_OK)
