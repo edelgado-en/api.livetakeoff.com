fa862b9d14b020044a9def730f24a5f7216ac914
Enrique Delgado <edelga34@ford.com>
2022-12-06 08:10:09 -0500

    Only showing open jobs for customer in customerHome

--- api/views/jobs.py
+++ api/views/jobs.py
@@ -97,11 +97,10 @@ class JobListView(ListAPIView):
                                | Q(purchase_order__icontains=searchText)
                               )
 
-            if status != 'All':
-                if status == 'O':
-                    qs = qs.filter(Q(status='A') | Q(status='S') | Q(status='U') | Q(status='W') | Q(status='R'))
-                else:
-                    qs = qs.filter(status=status)
+            if status == 'All':
+                qs = qs.filter(Q(status='A') | Q(status='S') | Q(status='U') | Q(status='W') | Q(status='R'))
+            else:
+                qs = qs.filter(status=status)
 
 
             if airport != 'All':
