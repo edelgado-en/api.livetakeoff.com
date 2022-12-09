a499d29877d74ebee7b0810212935cd1b247ce3f
Enrique Delgado <edelga34@ford.com>
2022-12-09 08:05:15 -0500

    Added date range filters to user productivity dashboard

--- api/views/user_productivity.py
+++ api/views/user_productivity.py
@@ -27,80 +27,129 @@ class UserProductivityView(APIView):
     permission_classes = (permissions.IsAuthenticated,)
 
 
-    def get(self, request, id):
+    def post(self, request, id):
         if not self.can_view_dashboard(request.user):
             return Response({'error': 'You do not have permission to view this page'}, status=status.HTTP_403_FORBIDDEN)
 
-        # get the user by id
         user_profile = UserProfile.objects.get(user_id=id)
-
         user = user_profile.user
-
-        # Count how many services the user has completed by querying the serviceActivity table
-        # and filtering by the user and the status of the service
-        # and then counting the number of services
-        services_completed = ServiceActivity.objects.filter(Q(project_manager=user) & Q(status='C')).count()
-
-        #Count how many retainer services the user has completed by querying the retainerServiceActivity table
-        # and filtering by the user and the status of the service
-        # and then counting the number of services
-        retainer_services_completed = RetainerServiceActivity.objects.filter(Q(project_manager=user) & Q(status='C')).count()
-
-        #Count how many jobs the user has completed by querying the jobStatusActivity table
-        # and filtering by the user and the status of the job
-        # and then counting the number of jobs
-        jobs_completed = JobStatusActivity.objects.filter(Q(user=user) & Q(status='C')).count()
-
-        # count how many jobPhotos the user has uploaded by querying the jobPhotos table
-        # and filtering by the user
-        # and then counting the number of photos
-        # This needs to come from JobStatusActivity table activity_type = 'U'
-        photos_uploaded = JobStatusActivity.objects.filter(Q(user=user) & Q(activity_type='U')).count()
-
-
-        #Count how many comments the user has made by querying the jobComments table
-        # and filtering by the user
-        # and then counting the number of comments
-        comments_created = JobComments.objects.filter(Q(author=user)).count()
-
-
-        # Get the date when this user was created by checking the User table
-        # and then converting the date to a string
-        #member_since = user.date_joined.strftime("%m/%d/%Y")
         member_since = user.date_joined
+        
+        last_service_date = ServiceActivity.objects.filter(Q(project_manager=user)
+                                                           & Q(status='C')).order_by('-timestamp').first()
+        
+        last_retainer_service_date = RetainerServiceActivity.objects.filter(Q(project_manager=user)
+                                                           & Q(status='C')).order_by('-timestamp').first()
+
+        dateSelected = request.data.get('dateSelected')
+
+        # get start date and end date based on the dateSelected value provided
+        if dateSelected == 'yesterday':
+            yesterday = datetime.now() - timedelta(days=1)
+            start_date = datetime(yesterday.year, yesterday.month, yesterday.day, 0, 0, 0)
+            end_date = datetime(yesterday.year, yesterday.month, yesterday.day, 23, 59, 59)
+        
+        elif dateSelected == 'last7Days':
+            today = date.today()
+            start_date = today - timedelta(days=7)
+            end_date = today
+
+        elif dateSelected == 'lastWeek':
+            today = date.today()
+            start_date = today - timedelta(days=today.weekday(), weeks=1)
+            end_date = start_date + timedelta(days=6)
+
+        elif dateSelected == 'MTD':
+            today = date.today()
+            start_date = date(today.year, today.month, 1)
+            end_date = datetime.now()
+
+        elif dateSelected == 'lastMonth':
+            today = date.today()
+            first = today.replace(day=1)
+            lastMonth = first - timedelta(days=1)
+            start_date = lastMonth.replace(day=1)
+            
+            # check if last month has 31 days or 30 days or 28 days
+            if lastMonth.month == 1 or lastMonth.month == 3 \
+                 or lastMonth.month == 5 or lastMonth.month == 7 \
+                 or lastMonth.month == 8 or lastMonth.month == 10 or lastMonth.month == 12:
+                
+                end_date = lastMonth.replace(day=31)
+            
+            elif lastMonth.month == 4 or lastMonth.month == 6 or lastMonth.month == 9 or lastMonth.month == 11:
+                end_date = lastMonth.replace(day=30)
+            
+            else:
+                end_date = lastMonth.replace(day=28)
+
+        elif dateSelected == 'lastQuarter':
+            today = date.today()
+            # get today's month and check it is in the first, second, third or fourth quarter.
+            #  if it is first quarter, then get the previous year's last quarter
+            if today.month in [1,2,3]:
+                start_date = date(today.year - 1, 10, 1)
+                end_date = date(today.year - 1, 12, 31)
+            
+            elif today.month in [4,5,6]:
+                start_date = date(today.year, 1, 1)
+                end_date = date(today.year, 3, 31)
+            
+            elif today.month in [7,8,9]:
+                start_date = date(today.year, 4, 1)
+                end_date = date(today.year, 6, 30)
+            
+            elif today.month in [10,11,12]:
+                start_date = date(today.year, 7, 1)
+                end_date = date(today.year, 9, 30)
+
+        
+        elif dateSelected == 'YTD':
+            year = datetime.now().year
+            start_date = datetime(year, 1, 1)
+            end_date = datetime.now()
+        
+        elif dateSelected == 'lastYear':
+            today = date.today()
+            start_date = date(today.year - 1, 1, 1)
+            end_date = date(today.year - 1, 12, 31)
 
 
-        # Get the date of the last service the user completed by querying the serviceActivity table
-        # and filtering by the user and the status of the service
-        # and then ordering by the timestamp of the service
-        # and then getting the first service in the list
-        # and then converting the date to a string
-        last_service_date = ServiceActivity.objects.filter(Q(project_manager=user) & Q(status='C')).order_by('-timestamp').first()
+
+        services_completed = ServiceActivity.objects.filter(Q(project_manager=user)
+                                                            & Q(status='C')
+                                                            & Q(timestamp__gte=start_date)
+                                                            & Q(timestamp__lte=end_date)).count()
+        
+        retainer_services_completed = RetainerServiceActivity.objects.filter(Q(project_manager=user)
+                                                            & Q(status='C')
+                                                            & Q(timestamp__gte=start_date)
+                                                            & Q(timestamp__lte=end_date)).count()
+        
+        jobs_completed = JobStatusActivity.objects.filter(Q(user=user)
+                                                            & Q(status='C')
+                                                            & Q(timestamp__gte=start_date)
+                                                            & Q(timestamp__lte=end_date)).count()
+        
+        photos_uploaded = JobStatusActivity.objects.filter(Q(user=user)
+                                                            & Q(activity_type='U')
+                                                            & Q(timestamp__gte=start_date)
+                                                            & Q(timestamp__lte=end_date)).count()
+        
+        comments_created = JobComments.objects.filter(Q(author=user)
+                                                            & Q(created__gte=start_date)
+                                                            & Q(created__lte=end_date)).count()
 
 
-        # Get the date of the last retainer service the user completed by querying the retainerServiceActivity table
-        # and filtering by the user and the status of the service
-        # and then ordering by the timestamp of the service
-        last_retainer_service_date = RetainerServiceActivity.objects.filter(Q(project_manager=user) & Q(status='C')).order_by('-timestamp').first()
-
-        # Get the last five jobComments the user has made by querying the jobComments table
-        # and filtering by the user
-        # and then ordering by the timestamp of the comment
-        # and then getting the first five comments in the list
-        last_five_comments = JobComments.objects.filter(Q(author=user)).order_by('-created')[:5]
-
-        # Get the top 5 services the user has completed by querying the serviceActivity table group by the service and airport
-        # and filtering by the user and the status of the service
-        # and then ordering by the number of services
-        # and then getting the first five services in the list
         top_five_services = ServiceActivity.objects \
                                             .values('service__name') \
                                             .annotate(count=Count('service__id')) \
-                                            .filter(Q(project_manager=user) & Q(status='C')) \
+                                            .filter(Q(project_manager=user) & Q(status='C')
+                                                    & Q(timestamp__gte=start_date)
+                                                    & Q(timestamp__lte=end_date)) \
                                             .order_by('-count')[:5]
 
         top_services = []
-        # add a percentage value to each service which is calculated based on the top 5 services provided
         for item in top_five_services:
             top_services.append({
                 'name': item['service__name'],
@@ -109,18 +158,16 @@ class UserProductivityView(APIView):
             })
 
         
-        # Get the top 5 retainer services the user has completed by querying the retainerServiceActivity table group by the service and airport
-        # and filtering by the user and the status of the service
-        # and then ordering by the number of services
-        # and then getting the first five services in the list
         top_five_retainer_services = RetainerServiceActivity.objects \
                                                             .values('retainer_service__name') \
                                                             .annotate(count=Count('retainer_service__id')) \
-                                                            .filter(Q(project_manager=user) & Q(status='C')) \
+                                                            .filter(Q(project_manager=user)
+                                                                    & Q(status='C')
+                                                                    & Q(timestamp__gte=start_date)
+                                                                    & Q(timestamp__lte=end_date) ) \
                                                             .order_by('-count')[:5]
 
         top_retainer_services = []
-        # add a percentage value to each retainer service which is calculated based on the top 5 retainer services provided
         for item in top_five_retainer_services:
             top_retainer_services.append({
                 'name': item['retainer_service__name'],
@@ -129,36 +176,32 @@ class UserProductivityView(APIView):
             })
 
         
-        # Get the top 5 aircarft types the user has completed services for by querying the serviceActivity table group by the job__aircraftType
-        # and filtering by the user and the status of the service
-        # and then ordering by the number of services
-        # and then getting the first five aircraft types in the list
-        # the resultset should look like this: aircraftType__name, services_completed
         top_five_aircraft_types = ServiceActivity.objects \
                                                 .values('job__aircraftType__name') \
                                                 .annotate(count=Count('job__aircraftType__id')) \
-                                                .filter(Q(project_manager=user) & Q(status='C')) \
+                                                .filter(Q(project_manager=user)
+                                                        & Q(status='C')
+                                                        & Q(timestamp__gte=start_date)
+                                                        & Q(timestamp__lte=end_date)) \
                                                 .order_by('-count')[:5]
 
-        # Get the top 5 airports the user has completed services for by querying the serviceActivity table group by the job__airport
-        # and filtering by the user and the status of the service
-        # and then ordering by the number of services
-        # and then getting the first five airports in the list
         top_five_airports = ServiceActivity.objects \
                                                 .values('job__airport__name') \
                                                 .annotate(count=Count('job__airport__id')) \
-                                                .filter(Q(project_manager=user) & Q(status='C')) \
+                                                .filter(Q(project_manager=user) & Q(status='C')
+                                                        & Q(timestamp__gte=start_date)
+                                                        & Q(timestamp__lte=end_date)) \
                                                 .order_by('-count')[:5]
 
-        # Get the last 100 services with status W by the user by querying the serviceActivity table
-        # and filtering by the user and the status of the service
-        # and then ordering by the timestamp of the service
-        # and then getting the first 100 services in the list
-        last_50_services = ServiceActivity.objects.filter(Q(project_manager=user) & Q(status='W')).order_by('-timestamp')[:50]
+
+        last_50_services = ServiceActivity.objects.filter(Q(project_manager=user)
+                                                        & Q(status='W')
+                                                        & Q(timestamp__gte=start_date)
+                                                        & Q(timestamp__lte=end_date)).order_by('-timestamp')[:50]
 
         recent_service_stats = []
 
-        # Iterate through last_100_services and get the corresponding service activity with status C for the same job and service
+        # Iterate through last_50_services and get the corresponding service activity with status C for the same job and service
         for service_activity in last_50_services:
             try:
                 service_activity_c = ServiceActivity.objects.filter(Q(job=service_activity.job)
@@ -176,19 +219,16 @@ class UserProductivityView(APIView):
                     if time_to_complete < 0:
                         time_to_complete = time_to_complete * -1
 
-                    #only append if an entry for this service_name and job_aircraftType_name does not already exist
                     if not any(d['service_name'] == service_name and d['job_aircraftType_name'] == job_aircraftType_name for d in recent_service_stats):
-                        recent_service_stats.append({'service_name': service_name, 'job_aircraftType_name': job_aircraftType_name, 'time_to_complete': time_to_complete})
-                    #recent_service_stats.append([service_name, job_aircraftType_name, time_to_complete])
+                        recent_service_stats.append({'service_name': service_name,
+                                                     'job_aircraftType_name': job_aircraftType_name,
+                                                     'time_to_complete': time_to_complete})
             
             except ServiceActivity.DoesNotExist:
                 continue
 
         
-        # create an array out of recent_service_stats where data is grouped by service_name.
-        #  Each entry of the array should look like this:
-        # {service: service_name, stats: [{"aircraft":job_aircraftType_name, "time_to_complete": time_to_complete}]
-        #  }
+        
         recent_service_stats_grouped = []
         for item in recent_service_stats:
             if not any(d['service'] == item['service_name'] for d in recent_service_stats_grouped):
@@ -196,7 +236,8 @@ class UserProductivityView(APIView):
             
             for item2 in recent_service_stats_grouped:
                 if item2['service'] == item['service_name']:
-                    item2['stats'].append({'aircraft': item['job_aircraftType_name'], 'time_to_complete': item['time_to_complete']})
+                    item2['stats'].append({'aircraft': item['job_aircraftType_name'],
+                                           'time_to_complete': item['time_to_complete']})
 
         # each entry in recent_service_stats_grouped should be sorted by time_to_complete
         for item in recent_service_stats_grouped:
@@ -205,16 +246,11 @@ class UserProductivityView(APIView):
         # convert time_to_complete in recent_service_stats_grouped to the following format: xh ym
         for item in recent_service_stats_grouped:
             for item2 in item['stats']:
-                item2['time_to_complete'] = '{0}h {1}m'.format(int(item2['time_to_complete']), int((item2['time_to_complete'] % 1) * 60))
+                item2['time_to_complete'] = '{0}h {1}m'.format(int(item2['time_to_complete']),
+                                                               int((item2['time_to_complete'] % 1) * 60))
 
-
-        comments = []
-        #iterate through last_five_comments and get the comment and date and append to comments list
-        for comment in last_five_comments:
-            comments.append([comment.comment, comment.created])
 
         vendor_name = ''
-
         if user_profile.vendor:
             vendor_name = user_profile.vendor.name
 
@@ -226,10 +262,11 @@ class UserProductivityView(APIView):
         }
 
         # total revenue generated
-        qs_service = ServiceActivity.objects.filter(
-                Q(status='C') &
-                Q(project_manager_id=user_profile.user)
-            ).values('service__id', 'job__id')
+        qs_service = ServiceActivity.objects.filter(Q(status='C')
+                                                    & Q(project_manager_id=user_profile.user)
+                                                    & Q(timestamp__gte=start_date)
+                                                    & Q(timestamp__lte=end_date)
+                                            ).values('service__id', 'job__id')
 
         total_revenue = 0
         for service in qs_service:
@@ -274,12 +311,10 @@ class UserProductivityView(APIView):
             'top_five_retainer_services': top_retainer_services,
             'top_five_aircraft_types': top_five_aircraft_types,
             'top_five_airports': top_five_airports,
-            'recent_service_stats': recent_service_stats_grouped,
-            'last_five_comments': comments,
+            'recent_service_stats': recent_service_stats_grouped
         }, status=status.HTTP_200_OK)
 
     
-
     def can_view_dashboard(self, user):
         if user.is_superuser or user.is_staff or user.groups.filter(name='Account Managers').exists():
             return True
