baec0891ff389718b47f89e391617a3987f8a451
Enrique Delgado <edelga34@ford.com>
2023-01-11 09:19:34 -0500

    Added role filter to users endpoint

--- api/views/users.py
+++ api/views/users.py
@@ -15,6 +15,9 @@ class UsersView(ListAPIView):
 
     def get_queryset(self):
         name = self.request.data.get('name')
+        # role can be project manager group, admin group, account manager group, or customer. A customer is determined by the customer field in the user profile.
+        # if the role is 'All', then do not filter by role
+        role = self.request.data.get('role')
 
         users = User.objects.filter(Q(username__icontains=name)
                                      | Q(first_name__icontains=name)
@@ -22,6 +25,17 @@ class UsersView(ListAPIView):
                             .filter(is_active=True) \
                             .select_related('profile') \
                             .order_by('first_name', 'last_name')
+
+        if role != 'All':
+            if role == 'Customers':
+                users = users.filter(profile__customer__isnull=False)
+            
+            elif role == 'Project Managers' or role == 'Account Managers':
+                users = users.filter(groups__name=role)
+            
+            elif role == 'Admins':
+                users = users.filter(is_staff=True)
+
 
         return users
 
