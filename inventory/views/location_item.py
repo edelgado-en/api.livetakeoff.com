b011e5f11909ee68298c09dd6a948006a13b4b5c
edelgado-git <enriquedelgado806@gmail.com>
2023-08-13 13:37:34 -0400

    Added functionality to move items

--- inventory/views/location_item.py
+++ inventory/views/location_item.py
@@ -26,7 +26,34 @@ class LocationItemView(APIView):
             location_item.status = 'U'
 
         elif action == 'move':
-            location_item.status = 'M'
+            # this is the location id you use to find the corresponding location item entry to increase the quantity with movingQuantity
+            destinationLocationId = request.data.get('destinationLocationId', None)
+
+            # this is the new quantity to update in the current location item
+            adjustedQuantity = request.data.get('adjustedQuantity', None)
+            
+            # this is the quantity that you add to the destionation location item
+            movingQuantity = request.data.get('movingQuantity', None)
+
+            # fetch location item for destination location and location_item.item
+            destination_location_item = LocationItem.objects.get(location_id=destinationLocationId, item=location_item.item)
+
+            # update destination location item quantity or create a new location item if it does not exits
+            if destination_location_item:
+                destination_location_item.quantity += movingQuantity
+                destination_location_item.save()
+            else:
+                destination_location_item = LocationItem.objects.create(location_id=destinationLocationId,
+                                                                        item=location_item.item,
+                                                                        quantity=movingQuantity,
+                                                                        status='U')
+
+            # update current location item quantity with the adjusted quantity
+            if adjustedQuantity < 0:
+                adjustedQuantity = 0
+
+            location_item.quantity = adjustedQuantity
+            location_item.status = 'U'
         
         else:
             return Response({'error': 'Invalid action'}, status=status.HTTP_400_BAD_REQUEST)
