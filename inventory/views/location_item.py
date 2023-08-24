25d52916fbb9c426d757ac2dcc20791c1a8ccfaf
edelgado-git <enriquedelgado806@gmail.com>
2023-08-24 08:29:34 -0400

    Added functionality to send emails to all admins when an item is out of stock or it reaches alert value

--- inventory/views/location_item.py
+++ inventory/views/location_item.py
@@ -4,10 +4,14 @@ from rest_framework import (permissions, status)
 from rest_framework .response import Response
 from rest_framework.views import APIView
 
+from django.contrib.auth.models import User
+
 from inventory.models import (
     LocationItem,
     LocationItemActivity
 )
+
+from api.email_util import EmailUtil
 
 class LocationItemView(APIView):
     permission_classes = (permissions.IsAuthenticated,)
@@ -31,14 +35,22 @@ class LocationItemView(APIView):
             quantity = request.data.get('quantity', None)
             quantity = int(quantity)
 
+            is_out_of_stock = False
+            is_threshold_met = False
+
+            delta_quantity = abs(quantity - location_item.quantity)
+            delta_cost = delta_quantity * location_item.item.cost_per_unit
+
             activity_type = 'A'
 
             if quantity < location_item.quantity:
                 activity_type = 'S'
-            
-            delta_quantity = abs(quantity - location_item.quantity)
 
-            delta_cost = delta_quantity * location_item.item.cost_per_unit
+                if quantity == 0:
+                    is_out_of_stock = True
+
+                elif location_item.threshold > 0 and quantity <= location_item.threshold:
+                    is_threshold_met = True
 
             location_item.quantity = quantity
             location_item.status = 'U'
@@ -50,6 +62,54 @@ class LocationItemView(APIView):
                                                 quantity=delta_quantity,
                                                 cost=delta_cost,
                                                 user=request.user)
+            
+            admins = User.objects.filter(Q(is_superuser=True) | Q(is_staff=True) | Q(groups__name='Account Managers'))
+
+            if is_out_of_stock:
+                title = f'OUT OF STOCK {location_item.item.name} at {location_item.location.name}'
+
+                body = f'''
+                <div style="text-align: center; font-size: 20px; font-weight: bold; margin-bottom: 20px;">OUT OF STOCK Item Notification</div>
+                
+                <div>
+                    <div style="padding:5px;font-weight: 700;">Item</div>
+                    <div style="padding:5px">{location_item.item.name}</div>
+                    <div style="padding:5px;font-weight: 700;">Location</div>
+                    <div style="padding:5px">{location_item.location.name}</div>
+                    <div style="padding:5px;font-weight: 700;">Quantity</div>
+                    <div style="padding:5px">OUT OF STOCK</div>
+                </div>
+                '''
+
+                email_util = EmailUtil()
+
+                for admin in admins:
+                    if admin.email:
+                        email_util.send_email(admin.email, title, body)
+            
+            elif is_threshold_met:
+                title = f'THRESHOLD MET {location_item.item.name} at {location_item.location.name}'
+
+                body = f'''
+                <div style="text-align: center; font-size: 20px; font-weight: bold; margin-bottom: 20px;">THRESHOLD Item Notification</div>
+                
+                <div>
+                    <div style="padding:5px;font-weight: 700;">Item</div>
+                    <div style="padding:5px">{location_item.item.name}</div>
+                    <div style="padding:5px;font-weight: 700;">Location</div>
+                    <div style="padding:5px">{location_item.location.name}</div>
+                    <div style="padding:5px;font-weight: 700;">Quantity</div>
+                    <div style="padding:5px">{location_item.quantity}</div>
+                    <div style="padding:5px;font-weight: 700;">Threshold</div>
+                    <div style="padding:5px">{location_item.threshold}</div>
+                </div>
+                '''
+
+                email_util = EmailUtil()
+
+                for admin in admins:
+                    if admin.email:
+                        email_util.send_email(admin.email, title, body)
 
         elif action == 'move':
             # this is the location id you use to find the corresponding location item entry to increase the quantity with movingQuantity
@@ -62,6 +122,8 @@ class LocationItemView(APIView):
             movingQuantity = request.data.get('movingQuantity', None)
 
             destination_location_item = None
+
+            # TODO: also add notificaitons when moving items
 
             # fetch location item for destination location and location_item.item
             try:
