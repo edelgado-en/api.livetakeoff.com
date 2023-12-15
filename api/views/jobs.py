d9c40d651bcdab2cef062fefe0daa73f9f34dcd2
edelgado-git <enriquedelgado806@gmail.com>
2023-12-15 07:30:48 -0500

    Adjusted jobs endpoint to account for multiple user groups

--- api/views/jobs.py
+++ api/views/jobs.py
@@ -96,80 +96,6 @@ class JobListView(ListAPIView):
             return qs
 
 
-        #########################################
-        # CUSTOMER
-
-        user_profile = self.request.user.profile
-
-        if user_profile and user_profile.customer:
-            searchText = self.request.data.get('searchText')
-            status = self.request.data.get('status')
-            airport = self.request.data.get('airport')
-
-            qs = Job.objects.filter(customer_id=user_profile.customer.id)
-
-            if searchText:
-                qs = qs.filter(Q(tailNumber__icontains=searchText)
-                               | Q(purchase_order__icontains=searchText)
-                              )
-
-            if status == 'All':
-                qs = qs.filter(Q(status='A') | Q(status='S') | Q(status='U') | Q(status='W') | Q(status='R'))
-            else:
-                qs = qs.filter(status=status)
-
-
-            if airport != 'All':
-                qs = qs.filter(airport_id=airport)
-
-
-            sortField = self.request.data.get('sortField')
-            if sortField == 'requestDate':
-                qs = qs.order_by(F('requestDate').desc(nulls_last=True))
-            
-            elif sortField == 'completeBy':
-                qs = qs.order_by(F('completeBy').asc(nulls_last=True))
-
-            elif sortField == 'arrivalDate':
-                qs = qs.order_by(F('on_site').desc(), F('estimatedETA').asc(nulls_last=True))
-
-
-            return qs
-
-
-        #########################################
-        # PROJECT MANAGER
-
-        if self.request.user.groups.filter(name='Project Managers').exists():
-            job_ids = set()
-
-            # Get job ids from pending services/retainer_services assigned to the current user
-            # If you have at least one pending service assigned to you, you can see the job
-            assignments = JobServiceAssignment.objects \
-                                            .select_related('job') \
-                                            .filter(project_manager=self.request.user.id) \
-                                            .all()
-
-            for assignment in assignments:
-                if assignment.status != 'C':
-                    job_ids.add(assignment.job.id)
-            
-            retainer_assignment = JobRetainerServiceAssignment.objects \
-                                                            .select_related('job') \
-                                                            .filter(project_manager=self.request.user.id) \
-                                                            .all()
-
-            for assignment in retainer_assignment:
-                if assignment.status != 'C':
-                    job_ids.add(assignment.job.id)
-
-            # the only statuses that a PM can see is Assigned and WIP
-            return Job.objects \
-                    .filter(Q(status='S') | Q(status='W')) \
-                    .filter(id__in=job_ids) \
-                    .order_by(F('completeBy').asc(nulls_last=True)) \
-                    .all()
-
         if self.request.user.groups.filter(name='Internal Coordinators').exists():
             searchText = self.request.data.get('searchText')
             status = self.request.data.get('status')
@@ -243,6 +169,79 @@ class JobListView(ListAPIView):
 
 
             return qs
+
+
+        if self.request.user.groups.filter(name='Project Managers').exists():
+            job_ids = set()
+
+            # Get job ids from pending services/retainer_services assigned to the current user
+            # If you have at least one pending service assigned to you, you can see the job
+            assignments = JobServiceAssignment.objects \
+                                            .select_related('job') \
+                                            .filter(project_manager=self.request.user.id) \
+                                            .all()
+
+            for assignment in assignments:
+                if assignment.status != 'C':
+                    job_ids.add(assignment.job.id)
+            
+            retainer_assignment = JobRetainerServiceAssignment.objects \
+                                                            .select_related('job') \
+                                                            .filter(project_manager=self.request.user.id) \
+                                                            .all()
+
+            for assignment in retainer_assignment:
+                if assignment.status != 'C':
+                    job_ids.add(assignment.job.id)
+
+            # the only statuses that a PM can see is Assigned and WIP
+            return Job.objects \
+                    .filter(Q(status='S') | Q(status='W')) \
+                    .filter(id__in=job_ids) \
+                    .order_by(F('completeBy').asc(nulls_last=True)) \
+                    .all()
+
+
+        #########################################
+        # CUSTOMER
+
+        user_profile = self.request.user.profile
+
+        if user_profile and user_profile.customer:
+            searchText = self.request.data.get('searchText')
+            status = self.request.data.get('status')
+            airport = self.request.data.get('airport')
+
+            qs = Job.objects.filter(customer_id=user_profile.customer.id)
+
+            if searchText:
+                qs = qs.filter(Q(tailNumber__icontains=searchText)
+                               | Q(purchase_order__icontains=searchText)
+                              )
+
+            if status == 'All':
+                qs = qs.filter(Q(status='A') | Q(status='S') | Q(status='U') | Q(status='W') | Q(status='R'))
+            else:
+                qs = qs.filter(status=status)
+
+
+            if airport != 'All':
+                qs = qs.filter(airport_id=airport)
+
+
+            sortField = self.request.data.get('sortField')
+            if sortField == 'requestDate':
+                qs = qs.order_by(F('requestDate').desc(nulls_last=True))
+            
+            elif sortField == 'completeBy':
+                qs = qs.order_by(F('completeBy').asc(nulls_last=True))
+
+            elif sortField == 'arrivalDate':
+                qs = qs.order_by(F('on_site').desc(), F('estimatedETA').asc(nulls_last=True))
+
+
+            return qs
+
 
 
     def post(self, request, *args, **kwargs):
