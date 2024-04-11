f320f496694aad00b1e03416b2b0f7cb6a56d508
edelgado-git <enriquedelgado806@gmail.com>
2024-04-11 17:14:31 -0400

    Added more fees to airport fees

--- api/views/airport_customer_fees.py
+++ api/views/airport_customer_fees.py
@@ -3,7 +3,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from api.models import (
-    CustomerAdditionalFeeAirport
+    CustomerAdditionalFeeAirport,
+    CustomerAdditionalFeeVendor,
 )
 
 class AirportCustomerFeesView(APIView):
@@ -12,13 +13,27 @@ class AirportCustomerFeesView(APIView):
         airport_id = request.data.get('airport_id')
         customer_id = request.data.get('customer_id')
 
-        fees = CustomerAdditionalFeeAirport.objects.filter(customer_additional_fee__customer_setting__customer_id=customer_id,
+        travel_fees = CustomerAdditionalFeeAirport.objects.filter(customer_additional_fee__customer_setting__customer_id=customer_id,
                                                            customer_additional_fee__type='A',
                                                            airport_id=airport_id).all()
         fees_dtos = []
 
-        for fee in fees:
+        for fee in travel_fees:
             f = {
+                'type': 'A',
+                'fee': fee.customer_additional_fee.fee,
+                'is_percentage': fee.customer_additional_fee.percentage,
+            }
+
+            fees_dtos.append(f)
+
+        vendor_higher_price_fees = CustomerAdditionalFeeVendor.objects.filter(customer_additional_fee__customer_setting__customer_id=customer_id,
+                                                           customer_additional_fee__type='V',
+                                                           airport_id=airport_id).all()
+
+        for fee in vendor_higher_price_fees:
+            f = {
+                'type': 'V',
                 'fee': fee.customer_additional_fee.fee,
                 'is_percentage': fee.customer_additional_fee.percentage,
             }
