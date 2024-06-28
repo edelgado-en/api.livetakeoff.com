20d0e7cd73f1cf9592b872360079f52e1e40b218
edelgado-git <enriquedelgado806@gmail.com>
2024-06-28 07:28:02 -0400

    Added unread comment alert to completed jobs view

--- api/serializers/job_completed.py
+++ api/serializers/job_completed.py
@@ -3,7 +3,7 @@ from .aircraft_type import AircraftTypeSerializer
 from .airport import AirportSerializer
 from .fbo import FBOSerializer
 from .customer import CustomerSerializer
-from ..models import Job
+from ..models import (Job, JobCommentCheck, JobComments)
 
 class JobCompletedSerializer(serializers.ModelSerializer):
     id = serializers.ReadOnlyField()
@@ -16,6 +16,27 @@ class JobCompletedSerializer(serializers.ModelSerializer):
     estimatedETD = serializers.DateTimeField(format="%m/%d/%y %H:%M")
     requestDate = serializers.DateTimeField(format="%m/%d/%y %H:%M", read_only=True)
     completion_date = serializers.DateTimeField(format="%m/%d/%y %H:%M", read_only=True)
+    comments_count = serializers.SerializerMethodField()
+
+    def get_comments_count(self, obj):
+        comments_count = 0
+
+        request = self.context.get('request')
+        
+        if request:
+            try:
+                job_comment_check = JobCommentCheck.objects.get(job=obj, user=request.user)
+                qs = JobComments.objects.filter(job=obj, created__gt=job_comment_check.last_time_check).exclude(author=self.context['request'].user)
+                if request.user.profile.customer and request.user.profile.customer == obj.customer:
+                    qs = qs.filter(is_public=True)
+                comments_count = qs.count()
+            except JobCommentCheck.DoesNotExist:
+                qs = JobComments.objects.filter(job=obj).exclude(author=request.user)
+                if request.user.profile.customer and request.user.profile.customer == obj.customer:
+                    qs = qs.filter(is_public=True)
+                comments_count = qs.count()
+        
+        return comments_count
 
     class Meta:
         model = Job
@@ -45,5 +66,6 @@ class JobCompletedSerializer(serializers.ModelSerializer):
             'management_fees_amount_applied',
             'arrival_formatted_date',
             'departure_formatted_date',
-            'complete_before_formatted_date'
+            'complete_before_formatted_date',
+            'comments_count'
             )
