4d64a12fbd81a55c02ee36470bf4c14cf147053b
edelgado-git <enriquedelgado806@gmail.com>
2025-07-09 09:07:26 -0400

    removed delay for flight based cleaning update

--- api/views/flight_based_cleaning_update.py
+++ api/views/flight_based_cleaning_update.py
@@ -47,8 +47,8 @@ class FlightBasedCleaningUpdateView(APIView):
 
                 flights_response = FlightawareApiService().get_flight_info(ident_to_use, None)
 
-                # Add a 2 seconds delay to avoid hitting the API rate limit
-                time.sleep(2)
+                # Add a 1 seconds delay to avoid hitting the API rate limit
+                #time.sleep(1)
 
                 if flights_response is None:
                     continue
